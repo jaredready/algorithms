1d2d10a6bb359eb405d117bb8a4d26c63d856fb0
Jared Ready <jared.ready1@gmail.com>
2018-08-08 20:50:41 -0500

    Add quick sort and reorganize repository

---
+++ quick-sort/cs/QuickSort/Sorter.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    public static class Sorter
+    {
+        /// <summary>
+        /// Determines if a list is sorted ascending
+        /// </summary>
+        /// <param name="list">The list to check if sorted</param>
+        /// <returns>True if the list is sorted ascending. Returns false if the list is not sorted or if the list is null or empty</returns>
+        public static bool ListIsSortedAscending(List<float> list)
+        {
+            if (list == null || list.Count < 1)
+            {
+                return false;
+            }
+            for (int i = 1; i < list.Count(); i++)
+            {
+                if (list.ElementAt(i).CompareTo(list.ElementAt(i - 1)) == -1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Sorts the given list in ascending order.
+        /// </summary>
+        /// <param name="list">The list to be sorted. Will not be altered.</param>
+        /// <returns>A sorted list containing the elements and only the elements pass in.</returns>
+        public static List<float> Sort(List<float> list)
+        {
+            if (list == null || list.Count <= 1)
+            {
+                return list;
+            }
+
+            List<float> sortedList = new List<float>(list);
+            QuickSort(sortedList, 0, list.Count() - 1);
+
+            return sortedList;
+        }
+
+        static void QuickSort(List<float> list, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = Partition(list, left, right);
+                QuickSort(list, left, pivotIndex - 1);
+                QuickSort(list, pivotIndex + 1, right);
+            }
+        }
+
+        static int Partition(List<float> list, int low, int high)
+        {
+            float pivot = list[low];
+            int left = low + 1;
+            int right = high;
+
+            while (left <= right)
+            {
+                if (list.ElementAt(right) > pivot)
+                {
+                    right--;
+                }
+                else if (list.ElementAt(left) <= pivot)
+                {
+                    left++;
+                }
+                else if (right < left)
+                {
+                    break;
+                }
+                else
+                {
+                    Swap(list, left, right);
+                }
+            }
+
+            Swap(list, low, right);
+            return right;
+        }
+
+        private static void Swap(List<float> list, int i, int j)
+        {
+            float swapTemp = list[i];
+            list[i] = list[j];
+            list[j] = swapTemp;
+        }
+    }
+}
---
+++ quick-sort/cs/SortTest/SorterTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using QuickSort;
+
+namespace SortTest
+{
+    [TestClass]
+    public class SorterTest
+    {
+        [TestMethod]
+        public void ListIsSortedAscending_SortedListNoDuplicates_ReturnsTrue()
+        {
+            List<float> sortedList = new List<float>(new float[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f });
+            Assert.IsTrue(Sorter.ListIsSortedAscending(sortedList));
+        }
+
+        [TestMethod]
+        public void ListIsSortedAscending_SortedListSomeDuplicates_ReturnsTrue()
+        {
+            List<float> sortedList = new List<float>(new float[] { 1.0f, 1.0f, 3.0f, 3.0f, 5.0f });
+            Assert.IsTrue(Sorter.ListIsSortedAscending(sortedList));
+        }
+
+        [TestMethod]
+        public void ListIsSortedAscending_AllDuplicates_ReturnsTrue()
+        {
+            List<float> list = new List<float>(new float[] { 1.1f, 1.1f, 1.1f, 1.1f, 1.1f });
+            Assert.IsTrue(Sorter.ListIsSortedAscending(list));
+        }
+
+        [TestMethod]
+        public void ListIsSortedAscending_UnsortedListNoDuplicates_ReturnsFalse()
+        {
+            List<float> unsortedList = new List<float>(new float[] { 3.0f, 1.0f, 7.0f, -1.0f });
+            Assert.IsFalse(Sorter.ListIsSortedAscending(unsortedList));
+        }
+
+        [TestMethod]
+        public void ListIsSortedAscending_UnsortedListSomeDuplicates_ReturnsFalse()
+        {
+            List<float> unsortedList = new List<float>(new float[] { 3.0f, 3.0f, 7.0f, -1.0f, -1.0f });
+            Assert.IsFalse(Sorter.ListIsSortedAscending(unsortedList));
+        }
+
+        [TestMethod]
+        public void ListIsSortedAscending_EmptyList_ReturnsFalse()
+        {
+            List<float> emptyList = new List<float>();
+            Assert.IsFalse(Sorter.ListIsSortedAscending(emptyList));
+        }
+
+        [TestMethod]
+        public void ListIsSortedAscending_NullList_ReturnsFalse()
+        {
+            List<float> emptyList = null;
+            Assert.IsFalse(Sorter.ListIsSortedAscending(emptyList));
+        }
+
+        [TestMethod]
+        public void Sort_UnsortedListNoDuplicates_ReturnsSortedList()
+        {
+            List<float> unsortedList = new List<float>(new float[] { 3.0f, 1.0f, 7.0f, -1.0f });
+
+            Assert.IsTrue(Sorter.ListIsSortedAscending(Sorter.Sort(unsortedList)));
+        }
+
+        [TestMethod]
+        public void Sort_UnsortedListSomeDuplicates_ReturnsSortedList()
+        {
+            List<float> unsortedList = new List<float>(new float[] { 3.0f, 1.0f, 7.0f, -1.0f, -1.0f, 3.0f });
+
+            Assert.IsTrue(Sorter.ListIsSortedAscending(Sorter.Sort(unsortedList)));
+        }
+
+        [TestMethod]
+        public void Sort_UnsortedListRandomValues_ReturnsSortedList()
+        {
+            List<float> randomList = RandomListOfFloats(10000);
+
+            Assert.IsTrue(Sorter.ListIsSortedAscending(Sorter.Sort(randomList)));
+        }
+
+        private List<float> RandomListOfFloats(int size)
+        {
+            // Referenced http://stackoverflow.com/a/3365388 to generate random floats
+
+            Random random = new Random();
+            List<float> randomList = new List<float>(size);
+            for(int i = 0; i < size; i++)
+            {
+                double mantissa = (random.NextDouble() * 2.0) - 1.0;
+                double exponent = Math.Pow(2.0, random.Next(-126, 128));
+                randomList.Add((float)(mantissa * exponent));
+            }
+
+            return randomList;
+        }
+    }
+}
